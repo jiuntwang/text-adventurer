57fa4da791a1e12c03f4f85b349e56e156ac7be4
Jiun Ting Wang <jiuntwang@gmail.com>
2017-08-01 19:50:13 -0700

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name textAdventurerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the textAdventurerApp
+ */
+angular.module('textAdventurerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
